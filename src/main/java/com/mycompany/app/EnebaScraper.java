25d434b1196a0514f517ff7d9702287333aaa56b
AmirKAT <amir-katal@hotmail.com>
2020-11-27 03:13:48 +0000

    Complete scraping file for Eneba website

--- src/main/java/com/mycompany/app/EnebaScraper.java
+++ src/main/java/com/mycompany/app/EnebaScraper.java
@@ -13,7 +13,7 @@ public class EnebaScraper extends WebScraper {
 		while(!stopThread) {			
 			try {
 				scrapeEnebaGameData();
-				sleep(3000);
+				sleep(6000);
 			}
 			catch(Exception ex) {
 				ex.printStackTrace();
@@ -22,54 +22,75 @@ public class EnebaScraper extends WebScraper {
 	}
 	
 	void scrapeEnebaGameData() throws IOException {
-		for(int a=1; a<100; a++) {
+		for(int a=1; a<500; a++) {
 			
-  		String url = "https://www.eneba.com/store?page=" + a + "&platforms[]=XBOX&regions[]=emea&regions[]=europe&regions[]=global&regions[]=united_kingdom&types[]=game";
+  		String url = "https://www.eneba.com/store?page=" + a + "&regions[]=emea&regions[]=europe&regions[]=global&regions[]=united_kingdom&types[]=game";
   				System.out.println("Scraping Eneba page " + a);
-  	
+
   		// Downloading HTML document from the site
 	  	Document doc = Jsoup.connect(url).get();
 	  	
 	  	Elements prods = doc.select("._3M7T08");
   	
-	  	// need: title, genre, age_rating, image, link, price, platform
+	  	// need: title, genre, image, link, price, platform
   	
-  	for(int i=0; i<prods.size(); ++i) {
+	  	for(int i=0; i<prods.size(); ++i) {
   		
-  		Elements prodWrappers = prods.select("._2rxjGA");
+	  		Elements prodWrappers = prods.select("._2rxjGA");
+	  		
+	  		for(int j=0; j<prodWrappers.size(); j++) {
+	  		
+	    		// Gets the title of the game 
+	    		Elements title = prodWrappers.get(j).select("._1ZwRcm");
+	    			System.out.println("Title: " + title.text());
+	    		
+	    		// Gets the image of the game
+	    		Elements image = prodWrappers.get(j).select("img");
+	    		
+	    		for(Element imageUrl : image) {
+	    			
+	    			String linkImage = imageUrl.attributes().get("src");
+	    				System.out.println("Image: " + linkImage );
+	    			
+	    		}// end of image link for loop
+	    		
+	    		// Gets the link of the game
+	    		Elements link = prodWrappers.get(j).select("._2idjXd");
+	    		
+	    		for(Element linkUrl : link) {
+	    			
+	    			String linkGame = linkUrl.attributes().get("href");
+	    				System.out.println("Link: " + linkGame );
+	    			
+	    			//scraping info from each linkGame
+	    			for(int b=0; b<link.size(); b++) {
+	    				
+	    				String url2 = "https://www.eneba.com" + linkGame;
+	    				
+	    				Document doc2 = Jsoup.connect(url2).get();
+	    				
+	    				Elements prods2 = doc2.select("._3MoLlD");
+	    				
+	    				for(int c=0; c<prods2.size(); c++) {
+	    					
+	    					Elements genre = prods2.get(c).select("._3w9_g5");
+	    						System.out.println("Genre: " + genre.text());
+	    					
+	    					Elements platform = prods2.get(c).select("._3Rlqdg");
+	    						System.out.println("Platform: " + platform.text());
+	    					
+	    				}
+	    			}//end of url2 for loop
+	    			
+	    		}// end of game link for loop
+	
+	    		// Gets the price of the game
+	    		Elements price = prodWrappers.get(j).select("._3RZkEb");
+	    			System.out.println("Price: " + price.text() + "\n");
+	    		
+	  		}//end of 3rd for loop
   		
-  		for(int j=0; j<prodWrappers.size(); j++) {
-  		
-    		// Gets the title of the game 
-    		Elements title = prodWrappers.get(j).select("._1ZwRcm");
-    		
-    		// Gets the image of the game
-    		Elements image = prodWrappers.get(j).select("img");
-    		
-    		for(Element imageUrl : image) {
-    			String linkImage = imageUrl.attributes().get("src");
-    			
-    			System.out.println("Image: " + linkImage );
-    		}// end of image link for loop
-    		
-    		// Gets the link of the game
-    		Elements link = prodWrappers.get(j).select("._2idjXd");
-    		
-    		for(Element linkUrl : link) {
-    			String linkGame = linkUrl.attributes().get("href");
-    			
-    			System.out.println("Link: " + linkGame );
-    		}// end of game link for loop
-
-    		// Gets the price of the game
-    		Elements price = prodWrappers.get(j).select("._3RZkEb");
-    		
-    		// testing output
-    		System.out.println("Title: " + title.text() + "\nPrice: " + price.text() + "\n");
-    		
-  		}//end of 3rd for loop
-  		
-  	}//end of 2nd for loop
+	  	}//end of 2nd for loop
   	
     }//end of 1st for loop
 		
