8337018dea2cf13b7438337a66ee37f0a0574d34
AmirKAT <amir-katal@hotmail.com>
2020-12-03 14:20:53 +0000

    Add 5 website working website scrapers

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,37 +3,38 @@ package com.mycompany.app;
 public class App {
 	
     public static void main( String[] args ) {
-//    	try {
-//    		EnebaScraper enebaScraper = new EnebaScraper();
-//				enebaScraper.start();
-//			}
-//    	catch(Exception ex) {
-//    		ex.printStackTrace();
-//    	}
-//    	
-//    	try {
-//				MmogaScraper mmogaScraper = new MmogaScraper();
-//				mmogaScraper.start();
-//			}
-//    	catch(Exception ex) {
-//    		ex.printStackTrace();
-//    	}
-//
-//    	try {
-//				GamingDragonScraper gamingDragonScraper = new GamingDragonScraper();
-//				gamingDragonScraper.start();
-//			}
-//    	catch(Exception ex) {
-//    		ex.printStackTrace();
-//    	}
-//    	
-//    	try {
-//				AmazonGamesScraper AmazonGamesScraper = new AmazonGamesScraper();
-//				AmazonGamesScraper.start();
-//			}
-//    	catch(Exception ex) {
-//    		ex.printStackTrace();
-//    	}
+    	try {
+    		EnebaScraper enebaScraper = new EnebaScraper();
+				enebaScraper.start();
+			}
+    	catch(Exception ex) {
+    		ex.printStackTrace();
+    	}
+    	
+    	try {
+				MmogaScraper mmogaScraper = new MmogaScraper();
+				mmogaScraper.start();
+			}
+    	catch(Exception ex) {
+    		ex.printStackTrace();
+    	}
+
+    	try {
+				GamingDragonScraper gamingDragonScraper = new GamingDragonScraper();
+				gamingDragonScraper.start();
+			}
+    	catch(Exception ex) {
+    		ex.printStackTrace();
+    	}
+    	
+    	try {
+				AmazonGamesScraper AmazonGamesScraper = new AmazonGamesScraper();
+				AmazonGamesScraper.start();
+			}
+    	catch(Exception ex) {
+    		ex.printStackTrace();
+    	}
+    	
     	try {
 				ZatuGamesScraper zatuGamesScraper = new ZatuGamesScraper();
 				zatuGamesScraper.start();
